0304e1250276d560eccd01c25e5474c22d0d79d5
Ahmad Shwaiki <ahmad.shwaiki@shahid.net>
2024-05-25 10:26:04 +0300

    feat: :art: add product card and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Footer from '@/app/ui/Footer';
 import Header from '@/app/ui/Header';
 import Navbar from '@/app/ui/Navbar';
+import ProductList from '@/app/ui/ProductList';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Navbar />
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 overflow-y-auto">
+        <ProductList />
       </main>
       <Footer />
     </div>
